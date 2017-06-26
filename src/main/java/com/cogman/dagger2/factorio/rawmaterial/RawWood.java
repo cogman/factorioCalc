c3942cb736f6e9373625beb81c28540cf6c94b3e
Thomas May <cogman10@hotmail.com>
2017-06-26 12:31:30 -0600

    Adding inject annotation to raw wood

--- src/main/java/com/cogman/dagger2/factorio/rawmaterial/RawWood.java
+++ src/main/java/com/cogman/dagger2/factorio/rawmaterial/RawWood.java
@@ -3,6 +3,7 @@ package com.cogman.dagger2.factorio.rawmaterial;
 import com.cogman.dagger2.factorio.Recipe;
 import com.cogman.dagger2.factorio.power.Fuel;
 
+import javax.inject.Inject;
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.Map;
@@ -10,6 +11,8 @@ import java.util.Map;
 public class RawWood implements Fuel {
     private static final BigDecimal JOULES = BigDecimal.valueOf(4_000_000);
 
+    @Inject RawWood() {}
+    
     @Override
     public Map<Recipe, BigDecimal> getRequirements() {
         return Collections.emptyMap();
